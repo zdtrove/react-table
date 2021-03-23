c2cef0022f45a2c45bf1b83253efbb9f82f373fb
Hoang An <antnh@icd-vn.com>
2021-03-23 16:24:28 +0700

    cm

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { StickyTable } from './components/StickyTable';
+
+function App() {
+  return (
+    <div>
+      <StickyTable />
+    </div>
+  );
+}
+
+export default App;
